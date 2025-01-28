02812b21050edf35b9201db7b2f6e0fe43740690
BARKHAC <49210263+BARKHAC@users.noreply.github.com>
2025-01-28 16:11:35 -0500

    added devise gem for auth, removed email field from devise migration for users as it was already present in users table
    added devise gem for auth, and removed email field from add devise migration rb, as it conflicted with initially created users table email field.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # config/environments/development.rb, adding below line to enable 'devise' for authentication.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
